7c3260c1d5fcafeee8a1dfb6b8c5c06e38c0d721
Sachit Karki <sachitjungkarki@gmail.com>
2020-04-18 14:53:54 +0545

    07 View Methods, Display & Search

--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,69 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices, bedroom_choices, state_choices
+
+from .models import Listing
 
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    return render(request, 'listings/listings.html', {
+        'listings': paged_listings
+    })
 
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    return render(request, 'listings/listing.html', {
+        'listing': listing
+    })
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(
+                city__iexact=city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(
+                state__iexact=state)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(
+                bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price)
+
+    return render(request, 'listings/search.html', {
+        'listings': queryset_list,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'search': request.GET
+    })
--- realtors/admin.py
+++ realtors/admin.py
@@ -3,9 +3,9 @@ from .models import Realtor
 
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email',
-                    'hire_date')
+    list_display = ('id', 'name', 'email', 'is_mvp', 'hire_date')
     list_display_links = ('id', 'name')
+    list_editable = ('is_mvp',)
     search_fields = ('name',)
     list_per_page = 25
 
